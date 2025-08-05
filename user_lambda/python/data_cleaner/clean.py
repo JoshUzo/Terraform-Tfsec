9bf5b054770c9b7d2875992c15726f0b3cd805e7
Joshua Uzoma <joshuauzoma@Joshuas-MBP.attlocal.net>
2025-08-05 15:41:39 -0500

    Changes so lambda and Layers can be zipped through CICD

---
+++ user_lambda/python/data_cleaner/clean.py
@@ -0,0 +1,20 @@
+from cleaner_utils import remove_nulls, normalize_keys, safe_cast
+
+def handler(event, context):
+    raw_data = {
+        "First Name": "Alice",
+        "Age": "30",
+        "Email": None,
+        "EmptyList": [],
+        "EmptyDict": {},
+    }
+
+    clean_data = remove_nulls(raw_data)
+    normalized = normalize_keys(clean_data)
+
+    age = safe_cast(normalized.get("age"), int, default=0)
+
+    return {
+        "clean_data": normalized,
+        "parsed_age": age
+    }
