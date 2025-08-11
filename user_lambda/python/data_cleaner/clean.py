77ca03076699e00011eace1027becd28411fb594
Joshua Uzoma <joshuauzoma@Joshuas-MBP.attlocal.net>
2025-08-11 11:25:25 -0500

    comment 82

--- user_lambda/python/data_cleaner/clean.py
+++ user_lambda/python/data_cleaner/clean.py
@@ -5,7 +5,7 @@ def handler(event, context):
         "First Name": "Alice",
         "Age": "30",
         "Email": None,
-        "EmptyList": ["first", "eigth"],
+        "EmptyList": ["first", "ninth"],
         "EmptyDict": {"help":"value6"},
     }
 
